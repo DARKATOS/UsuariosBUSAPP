a7da8b5e4cb2829eff7da794325ba54f39e3b169
DARKATOS <alejoved@yahoo.com.co>
2017-02-22 15:23:44 -0500

    Creacion del proyecto

---
+++ UsuariosBUSAPP/app/src/main/java/com/example/jorge_alejandro/usuariosbusapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jorge_alejandro.usuariosbusapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Aplicativo para los usuarios de BUSAPP
+    }
+}
