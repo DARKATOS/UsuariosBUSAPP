fec4bf36b0fb8b54693bfdea2d88f527c7867d10
DARKATOS <alejoved@yahoo.com.co>
2017-02-24 20:22:41 -0500

    Commit 24-02-2017 8:22

--- UsuariosBUSAPP/app/src/main/java/com/example/jorge_alejandro/usuariosbusapp/MainActivity.java
+++ UsuariosBUSAPP/app/src/main/java/com/example/jorge_alejandro/usuariosbusapp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.jorge_alejandro.usuariosbusapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapa=(Button)findViewById(R.id.button1);
+        mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(i);
+            }
+        });
         //Aplicativo para los usuarios de BUSAPP
     }
 }
